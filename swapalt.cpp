b7dfd38e4246c92ff41db1fb13974d035b17856e
Rishabh Mahla <rishabh.mahla@gmail.com>
2022-07-28 17:48:31 +0530

    Swap alternate numbers in array using cpp

---
+++ swapalt.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+//PROGRAM TO REVERSE AN ARRAY
+
+using namespace std;
+
+int swapalt(int arr[],int size){
+	
+		for(int i=0;i<size;i+=2){
+		if(i+1<size){
+		   swap(arr[i],arr[i+1]);
+		}
+	}	
+}
+int printalt(int arr[],int size){
+	cout<<"The reverse of the given array is [ ";
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<"]";
+	cout<<endl;
+}
+
+int main()
+{
+ int size;
+ cout<<"Enter the size of the array : ";
+ cin>>size;
+ int array[size];
+ for(int i=0;i<size;i++){
+ 	cout<<"Enter the element "<<i+1<<endl;
+ 	cin>>array[i];
+ }
+ 
+ swapalt(array,size);
+ printalt(array,size);
+ 
+ return 0;
+}
+
+
